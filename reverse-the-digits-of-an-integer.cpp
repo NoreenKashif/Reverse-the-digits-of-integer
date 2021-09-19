33aff1deba97478854ebbc2fd492145de5cde151
NoreenKashif <89309542+NoreenKashif@users.noreply.github.com>
2021-09-19 18:06:39 +0500

    Update reverse-the-digits-of-an-integer.cpp

--- reverse-the-digits-of-an-integer.cpp
+++ reverse-the-digits-of-an-integer.cpp
@@ -13,10 +13,11 @@ int main()
 	reverse = 0;//accumulator
 	while (num!=0)
 	{
-		d = num % 10;
+		d = num % 10;//giving right most digit
 		reverse = (reverse * 10) + d;
-		num = num / 10;
+		num = num / 10;//updating numbers once it's done for one digit and so on
 	}
-	cout << "\nReverse of " << num1 << " is : " << setw(n)<<setfill('0')<<reverse;
+	cout << "\nReverse of " << num1 << " is : " << setw(n)<<setfill('0')<<reverse;//in case if user enters 5000 or a number
+	//like that the program will return 0005
 	cout << endl;
 }
